710696b70e13ff9daa7916197586cb4bea0a1eca
Giacomo Tagliabue <giacomo.tag@gmail.com>
2017-03-26 18:50:37 -0400

    add icons

---
+++ src/ui/components/icon.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+import icons from '../assets/icons'
+import transform from '../utils/transform'
+
+const ICON_SIZE = 512
+const TRANSLATE_TO_CENTER = transform.translate(-ICON_SIZE / 2, -ICON_SIZE / 2)
+  .string()
+// tslint:disable-next-line:max-line-length
+export const ICON_BBOX = `${-ICON_SIZE / 2} ${-ICON_SIZE / 2} ${ICON_SIZE / 2} ${ICON_SIZE / 2}`
+
+type IProps = React.HTMLAttributes<{}> & React.SVGProps & {
+  icon: keyof typeof icons,
+}
+
+export default class Icon extends React.PureComponent<IProps, {}> {
+  render() {
+    const { icon, ...props } = this.props
+    const [background, ...elements] = icons[icon]
+
+    return (
+      <g {...props}>
+        <g transform={TRANSLATE_TO_CENTER}>
+          <path d={background} fill="transparent" stroke="transparent" />
+          <g>
+            {elements.map((d, idx) => <path d={d} key={idx} />)}
+          </g>
+        </g>
+      </g>
+    )
+  }
+}
